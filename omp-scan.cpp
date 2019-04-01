7309c99aeb3b5108ea9b5b4f4423923c0b053601
yc2945 <yc2945@nyu.edu>
2019-04-01 18:06:45 -0400

    finish scan

--- omp-scan.cpp
+++ omp-scan.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <omp.h>
+#include <stdlib.h>
 
 // Scan A array and write result into prefix_sum array;
 // use long data type to avoid overflow
@@ -14,11 +15,57 @@ void scan_seq(long* prefix_sum, const long* A, long n) {
 }
 
 void scan_omp(long* prefix_sum, const long* A, long n) {
-  // TODO: implement multi-threaded OpenMP scan
+
+  int nthreads; 
+  int chunk_size; 
+  #pragma omp parallel 
+  {
+    nthreads = omp_get_num_threads();
+    double size = (double) n / (double) nthreads;
+    double rounded = std::ceil(size);
+    chunk_size = (int) rounded;
+
+    #pragma omp for
+    for (int i = 0; i < nthreads; i++) {
+      int tid = omp_get_thread_num();
+
+      if (tid == 0) {
+        nthreads = omp_get_num_threads();
+        printf("Number of threads = %d\n", nthreads);
+      }
+      printf("Thread %d is starting...\n",tid);
+
+      int start = tid * chunk_size; 
+      int end; 
+      if (tid == nthreads - 1) {
+        end = n; 
+      } else {
+        end = start + chunk_size; 
+      }
+
+      for (int j = start; j < end; j++) {
+        if (j == 0) {
+          prefix_sum[j] = 0; 
+        } else {
+          prefix_sum[j] = A[j-1]; 
+          if (j != start) {
+            prefix_sum[j] += prefix_sum[j-1]; 
+          }
+        }
+      }
+    }
+  }
+
+  for (int i = chunk_size; i < n; i++) {
+    int chunk_id = i / chunk_size; 
+    prefix_sum[i] += prefix_sum[chunk_id * chunk_size - 1]; 
+  }
+
 }
 
 int main() {
-  long N = 100000000;
+  // long N = 100000000;
+  long N = 20; 
   long* A = (long*) malloc(N * sizeof(long));
   long* B0 = (long*) malloc(N * sizeof(long));
   long* B1 = (long*) malloc(N * sizeof(long));
